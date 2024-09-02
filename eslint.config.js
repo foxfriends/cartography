f51b535a93ebcc5e1606ba4e1cabc8e049b03397
Cameron Eldridge <cameldridge@gmail.com>
2024-09-01 22:41:29 -0400

    set up initial svelte 5 project, with tools upgraded to modern

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tsEslint from "typescript-eslint";
+import svelteEslint from "eslint-plugin-svelte";
+import svelteParser from "svelte-eslint-parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...svelteEslint.configs["flat/recommended"],
+  eslintConfigPrettier,
+  ...svelteEslint.configs["flat/prettier"],
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: "module",
+      globals: { ...globals.node, ...globals.browser },
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsEslint.parser,
+        extraFileExtensions: [".svelte"],
+      },
+    },
+  },
+  {
+    ignores: [
+      "**/.DS_Store",
+      "**/node_modules",
+      "build",
+      ".svelte-kit",
+      "package",
+      "**/.env",
+      "**/.env.*",
+      "!**/.env.example",
+      "**/package-lock.json",
+    ],
+  },
+);
