d8163c979b1f730176be1fcf661555442d5fd47d
Cameron Eldridge <cameldridge@gmail.com>
2024-09-04 21:31:16 -0400

    install seedrandom

---
+++ src/lib/Random.ts
@@ -0,0 +1,17 @@
+import seedrandom from "seedrandom";
+
+export class Random {
+  private rng: seedrandom.PRNG;
+
+  constructor(seed: string) {
+    this.rng = seedrandom(seed);
+  }
+
+  int(max: number): number {
+    return Math.floor(this.rng() * max);
+  }
+
+  choose<T>(options: T[]): T {
+    return options[this.int(options.length)];
+  }
+}
