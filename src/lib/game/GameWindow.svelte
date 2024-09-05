96ed586619b6eb46729574cf743a2fc97cd755a5
Cameron Eldridge <cameldridge@gmail.com>
2024-09-05 09:13:26 -0400

    put some terrain in the back

--- src/lib/game/GameWindow.svelte
+++ src/lib/game/GameWindow.svelte
@@ -1,9 +1,104 @@
 <script lang="ts">
+  const TILE_SIZE = 128;
+
   let clientWidth = $state(0);
   let clientHeight = $state(0);
 
   let offsetX = $state(0);
   let offsetY = $state(0);
+
+  const geography: {
+    biome: string;
+    origin: { x: number; y: number };
+    terrain: { type: string }[][];
+    resources: { id: string; x: number; y: number }[];
+  } = {
+    biome: "Coast",
+    origin: { x: 0, y: 0 },
+    terrain: [
+      [
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+      ],
+      [
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+      ],
+      [
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+      ],
+      [
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+      ],
+      [
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+      ],
+      [
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+      ],
+      [
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+      ],
+      [
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+        { type: "Grass" },
+      ],
+    ],
+    resources: [],
+  };
 
   const cards: { id: string; x: number; y: number }[] = [
     { id: "test-card", x: 0, y: 0 },
@@ -30,14 +125,29 @@
     offsetY -= event.movementY;
   }
 
+  const xMin = $derived(Math.floor(offsetX / TILE_SIZE));
+  const yMin = $derived(Math.floor(offsetY / TILE_SIZE));
+  const xMax = $derived(Math.floor((offsetX + clientWidth) / TILE_SIZE) + 1);
+  const yMax = $derived(Math.floor((offsetY + clientHeight) / TILE_SIZE) + 1);
+
   function onScreen({ x, y }: { x: number; y: number }): boolean {
     if (clientWidth === 0 || clientHeight === 0) return false;
-    const xMin = Math.floor(offsetX / 128);
-    const yMin = Math.floor(offsetY / 128);
-    const xMax = Math.floor((offsetX + clientWidth) / 128);
-    const yMax = Math.floor((offsetY + clientHeight) / 128);
-    return xMin <= x && x <= xMax && yMin <= y && y <= yMax;
+    return xMin <= x && x < xMax && yMin <= y && y < yMax;
   }
+
+  const terrain = $derived(
+    geography.terrain
+      .slice(Math.max(0, yMin - geography.origin.y), Math.max(0, yMax - geography.origin.y))
+      .flatMap((row, y) =>
+        row
+          .slice(Math.max(0, xMin - geography.origin.x), Math.max(0, xMax - geography.origin.x))
+          .map((col, x) => ({
+            x: x + Math.max(xMin - geography.origin.x, 0),
+            y: y + Math.max(yMin - geography.origin.y, 0),
+            ...col,
+          })),
+      ),
+  );
 </script>
 
 <div
@@ -48,8 +158,11 @@
   {onmousedown}
   role="presentation"
 >
+  {#each terrain as tile (tile)}
+    <div class="tile" data-type={tile.type} style="--grid-x: {tile.x}; --grid-y: {tile.y}"></div>
+  {/each}
   {#each cards.filter(onScreen) as card (card.id)}
-    <div class="card" style="--grid-x: {card.x}; --grid-y: {card.y}"></div>
+    <div class="card tile" style="--grid-x: {card.x}; --grid-y: {card.y}"></div>
   {/each}
 
   <div class="gridlines"></div>
@@ -63,10 +176,10 @@
     height: 100%;
     position: relative;
     overflow: hidden;
-    background-color: rgb(63 155 11);
+    background-color: rgb(255 255 255);
   }
 
-  .card {
+  .tile {
     position: absolute;
     top: 0;
     left: 0;
@@ -76,6 +189,13 @@
       calc(var(--grid-x) * var(--card-size) - var(--offset-x)),
       calc(var(--grid-y) * var(--card-size) - var(--offset-y))
     );
+
+    &[data-type="Grass"] {
+      background-color: rgb(63 155 11);
+    }
+  }
+
+  .card {
     background-color: white;
   }
 
