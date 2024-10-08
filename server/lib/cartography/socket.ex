4cfcdd06cfe12a009c211605df20a8515bf14997
Cameron Eldridge <cameldridge@gmail.com>
2024-10-08 17:46:14 -0400

    fixing ci

--- server/lib/cartography/socket.ex
+++ server/lib/cartography/socket.ex
@@ -23,7 +23,7 @@ defmodule Cartography.Socket do
   def handle_json(%{"type" => "auth", "id" => id}, %{account_id: nil} = state)
       when is_binary(id) do
     with {:ok, _} <- Cartography.Repo.insert(%Cartography.Account{id: id}, on_conflict: :nothing),
-         %Cartography.Account{id: id} = account <- Cartography.Repo.get!(Cartography.Account, id),
+         %Cartography.Account{id: id} <- Cartography.Repo.get!(Cartography.Account, id),
          do: {:push, {:json, %{id: id}}, %{state | account_id: id}}
   end
 end
