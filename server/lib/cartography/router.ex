c3c6693bf11bdae941fc3751226061c984920094
Cameron Eldridge <cameldridge@gmail.com>
2024-10-07 22:33:32 -0400

    set up a websocket protocol negotiation

--- server/lib/cartography/router.ex
+++ server/lib/cartography/router.ex
@@ -5,9 +5,36 @@ defmodule Cartography.Router do
   plug(:match)
   plug(:dispatch)
 
+  def accept_only([], _), do: :unknown
+  def accept_only([protocol | _], protocol), do: {:ok, protocol}
+  def accept_only([_ | protocols], protocol), do: accept_only(protocols, protocol)
+
+  def accept(_, []), do: :unknown
+
+  def accept(protocols, [protocol | non_preferred]) do
+    case accept_only(protocols, protocol) do
+      :unknown -> accept(protocols, non_preferred)
+      {:ok, protocol} -> {:ok, protocol}
+    end
+  end
+
+  def negotiate_protocol(conn) do
+    conn |> get_req_header("sec-websocket-protocol") |> accept(["cartography-v1"])
+  end
+
+  def upgrade(conn) do
+    case negotiate_protocol(conn) do
+      {:ok, "cartography-v1"} ->
+        WebSockAdapter.upgrade(conn, Cartography.Socket, [], timeout: 60_000)
+
+      :unknown ->
+        send_resp(conn, 400, "No supported protocol requested")
+    end
+  end
+
   get "/websocket" do
     conn
-    |> WebSockAdapter.upgrade(Cartography.Socket, [], timeout: 60_000)
+    |> upgrade()
     |> halt()
   end
 
--- server/lib/cartography/socket.ex
+++ server/lib/cartography/socket.ex
@@ -3,8 +3,12 @@ defmodule Cartography.Socket do
   WebSocket handler for all game clients.
   """
 
-  def init(options) do
-    {:ok, options}
+  defmodule State do
+    defstruct []
+  end
+
+  def init(_) do
+    {:ok, %State{}}
   end
 
   def handle_in({"ping", [opcode: :text]}, state) do
