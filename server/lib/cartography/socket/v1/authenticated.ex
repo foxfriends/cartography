81acffeb1c33509a30657469b7c8c84b063eb3ef
Cameron Eldridge <cameldridge@gmail.com>
2024-10-10 16:52:07 -0400

    lint

--- server/lib/cartography/socket/v1/authenticated.ex
+++ server/lib/cartography/socket/v1/authenticated.ex
@@ -5,9 +5,11 @@ defmodule Cartography.Socket.V1.Authenticated do
 
   def handle_message(
         "place_card",
-        %{"card_id" => card_id, "field_id" => field_id, "x" => _x, "y" => _y},
+        %{"card_id" => _card_id, "field_id" => _field_id, "x" => _x, "y" => _y},
         %{account_id: account_id} = state
       ) do
-    {:ok, state}
+    Cartography.Repo.as_account_id(account_id, fn ->
+      {:ok, state}
+    end)
   end
 end
