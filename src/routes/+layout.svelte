ebac5654a34335a1baa3634127b3e993ae90f782
Cameron Eldridge <cameldridge@gmail.com>
2026-01-31 17:26:37 -0500

    trying out adding glitchtip (sentry)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
   import "core-js/actual/iterator";
+  import * as Sentry from "@sentry/browser";
   import type { Snippet } from "svelte";
+  import Package from "../../package.json";
+  import { PUBLIC_SENTRY_DSN, PUBLIC_ENV } from "$env/static/public";
+
+  if (PUBLIC_SENTRY_DSN) {
+    Sentry.init({
+      dsn: PUBLIC_SENTRY_DSN,
+      release: Package.version,
+      environment: PUBLIC_ENV,
+    });
+  }
 
   const { children }: { children: Snippet } = $props();
 </script>
